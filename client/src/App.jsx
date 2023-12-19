5a7dc586be6c4e158799ecf6a2e54bcf40239285
raylaraya <raymondlaraya@gmail.com>
2023-12-18 20:49:53 -0500

    Style navbar and add navbar menu animation When the screen width is less than 940px the navbar menu will not be displayed and the hamburger menu will appear. When the user toggles the hamburger menu the navbar menu will appear. Only issue is navbar menu is not centered when screen width is more than 940px. Commiting code now to have a copy of this code before making changes to fix navbar menu being cetered.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/homePage/Home";
 import Appointments from "./pages/appointmentsPage/Appointments";
-import Login from "./pages/loginPage/Login";
 import About from "./pages/aboutPage/About";
 import Gallery from "./pages/galleryPage/Gallery";
 import Services from "./pages/servicesPage/Services";
+import Contact from "./pages/contactPage/Contact";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/appointments" element={<Appointments />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/services" element={<Services />} />
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
@@ -6,36 +6,76 @@ import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
 
+  // We'll use inline styles to add or remove the transition effect
+  const navbarMenuStyle = {
+    transform: isNavExpanded ? "translateX(0)" : "translateX(-100%)",
+    // We only want the transition to apply when the menu state changes, not on initial render
+    transition: isNavExpanded !== null ? "transform 0.5s ease-out" : "none",
+  };
+
+  const toggleNav = () => {
+    setIsNavExpanded((prevExpand) => !prevExpand);
+  };
+
+  // const toggleNav = () => {
+  //   if (!isNavExpanded) {
+  //     // Enable the animation when expanding the menu
+  //     navbarMenuRef.current.classList.add("animate");
+  //   } else {
+  //     // Remove the animation when collapsing the menu
+  //     // The transition will still occur due to teh existing CSS rules
+  //     setTimeout(() => {
+  //       navbarMenuRef.current.classList.remove("animate");
+  //     }, 500);
+  //   }
+  //   setIsNavExpanded(!isNavExpanded);
+  // };
+
+  // This effect runs once on mount and unmount
+  // useEffect(() => {
+  //   return () => {
+  //     if (navbarMenuRef.current) {
+  //       navbarMenuRef.current.classList.remove("animate");
+  //     }
+  //   };
+  // }, []);
+
   return (
     <nav className="navbar">
-      <div className="logo-container">
-        <img src="/AvexBarberLogo.PNG" alt="Avex Logo" className="logo-image" />
-      </div>
-      <ul className={`navbar-menu ${isNavExpanded ? "expanded" : ""}`}>
-        <li className="navbar-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/services">Services</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/appointments">Appointments</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/gallery">Gallery</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/about">About</Link>
-        </li>
-        <li className="navbar-item">
-          <Link to="/login">Login</Link>
-        </li>
-      </ul>
-      <div
-        className="hamburger"
-        onClick={() => setIsNavExpanded(!isNavExpanded)}
-      >
-        {isNavExpanded ? <FaTimes /> : <FaBars />}
+      <div className="navbar-container">
+        <Link to="/" className="logo-container">
+          <img
+            src="/AvexBarberLogo.PNG"
+            alt="Avex Logo"
+            className="logo-image"
+          />
+        </Link>
+        <ul className="navbar-menu" style={navbarMenuStyle}>
+          <li className="navbar-item">
+            <Link to="/">HOME</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/services">SERVICES</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/appointments">APPOINTMENTS</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/gallery">GALLERY</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/about">ABOUT</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/contact">CONTACT</Link>
+          </li>
+        </ul>
+        <Link to="/appointments" className="book-button">
+          BOOK
+        </Link>
+        <div className="hamburger" onClick={toggleNav}>
+          {isNavExpanded ? <FaTimes /> : <FaBars />}
+        </div>
       </div>
     </nav>
   );
