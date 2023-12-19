381db83b7c2fd7b11e160f3164608ae768539433
raylaraya <raymondlaraya@gmail.com>
2023-12-19 01:18:11 -0500

    Make small adjustments to navbar Could not fix the issue of the navbar menu transition happening when I resize the screen to less than 940px.

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
@@ -6,39 +6,9 @@ import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
 
-  // We'll use inline styles to add or remove the transition effect
-  const navbarMenuStyle = {
-    transform: isNavExpanded ? "translateX(0)" : "translateX(-100%)",
-    // We only want the transition to apply when the menu state changes, not on initial render
-    transition: isNavExpanded !== null ? "transform 0.5s ease-out" : "none",
+  const handleClick = () => {
+    setIsNavExpanded(!isNavExpanded);
   };
-
-  const toggleNav = () => {
-    setIsNavExpanded((prevExpand) => !prevExpand);
-  };
-
-  // const toggleNav = () => {
-  //   if (!isNavExpanded) {
-  //     // Enable the animation when expanding the menu
-  //     navbarMenuRef.current.classList.add("animate");
-  //   } else {
-  //     // Remove the animation when collapsing the menu
-  //     // The transition will still occur due to teh existing CSS rules
-  //     setTimeout(() => {
-  //       navbarMenuRef.current.classList.remove("animate");
-  //     }, 500);
-  //   }
-  //   setIsNavExpanded(!isNavExpanded);
-  // };
-
-  // This effect runs once on mount and unmount
-  // useEffect(() => {
-  //   return () => {
-  //     if (navbarMenuRef.current) {
-  //       navbarMenuRef.current.classList.remove("animate");
-  //     }
-  //   };
-  // }, []);
 
   return (
     <nav className="navbar">
@@ -50,7 +20,7 @@ const Navbar = () => {
             className="logo-image"
           />
         </Link>
-        <ul className="navbar-menu" style={navbarMenuStyle}>
+        <ul className={`navbar-menu ${isNavExpanded ? "expanded" : ""}`}>
           <li className="navbar-item">
             <Link to="/">HOME</Link>
           </li>
@@ -73,7 +43,7 @@ const Navbar = () => {
         <Link to="/appointments" className="book-button">
           BOOK
         </Link>
-        <div className="hamburger" onClick={toggleNav}>
+        <div className="hamburger" onClick={handleClick}>
           {isNavExpanded ? <FaTimes /> : <FaBars />}
         </div>
       </div>
