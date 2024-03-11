adc4913066db86b130e58ffa0dc0e08f06bedad9
raylaraya <raymondlaraya@gmail.com>
2024-03-11 18:15:04 -0400

    Implement logout functionality in navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { useAuth } from "../../context/AuthContext";
 
 const Navbar = () => {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const { user, logout } = useAuth();
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -12,6 +14,10 @@ const Navbar = () => {
 
   const handleClick = () => {
     setIsNavExpanded(!isNavExpanded);
+  };
+
+  const handleLogout = async () => {
+    await logout();
   };
 
   return (
@@ -35,9 +41,15 @@ const Navbar = () => {
             <Link to="/about">ABOUT</Link>
           </li>
         </ul>
-        <Link to="/login" className="login-button">
-          Login
-        </Link>
+        {user ? (
+          <button onClick={handleLogout} className="login-button">
+            Logout
+          </button>
+        ) : (
+          <Link to="/login" className="login-button">
+            Login
+          </Link>
+        )}
         <div className="hamburger" onClick={handleClick}>
           {isNavExpanded ? <FaTimes /> : <FaBars />}
         </div>
